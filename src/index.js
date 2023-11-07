09bd7779a3179017de1eb2f3b6ca88cb08075afa
Saranya Periyasamy <147969861+saranyamec@users.noreply.github.com>
2023-11-07 14:41:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './sty.css';
+import Add from './form';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+ <Add/>
+ </>
+
+);
+
